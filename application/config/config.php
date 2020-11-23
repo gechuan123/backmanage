89e2d5ea03131c4806525da76239f3e4e07ced85
DESKTOP-QSKKK56\tengbo <1261492506@QQ.com>
2020-11-23 11:25:46 +0800

    no message

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://xsight.sakura.ne.jp/aadmin';
-$config['base_url'] = 'http://192.168.1.53:8084';
+$config['base_url'] = 'http://www.backmanage.com/';
 /*
 |--------------------------------------------------------------------------
 | Index File
