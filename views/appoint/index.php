e954a8fdc7abd95cee6aa186daead80ddbc4e545
GECHUAN123\gechuan123 <15840508803@163.com>
2020-11-24 09:10:18 +0800

    no message

--- views/appoint/index.php
+++ views/appoint/index.php
@@ -1,7 +1,7 @@
 <?php $this->load->view('appoint/header');?>	
 <style type="text/css">
 .site-doc-icon{margin-bottom: 50px; font-size: 0;}
-.site-doc-icon li{display: inline-block; vertical-align: middle; width: 120px; line-height: 25px; padding: 20px 0; margin-right: -px; margin-bottom: -1px; border: 1px solid #e2e2e2; font-size: 14px; text-align: center; color: #666; transition: all .3s; -webkit-transition: all .3s;}
+.site-doc-icon li{display: inline-block; vertical-align: middle; width: 210px; line-height: 55px; padding: 20px 0; margin-right: -px; margin-bottom: -1px; border: 1px solid #e2e2e2; font-size: 14px; text-align: center; color: #666; transition: all .3s; -webkit-transition: all .3s;}
 .site-doc-icon li .layui-icon{display: inline-block; font-size: 36px;}
 .site-doc-icon li .fontclass{display: none;}
 .site-doc-icon li .name{color: #c2c2c2;}
@@ -51,55 +51,24 @@
 		</div>
 		<div class="layui-fluid">
 			<div class="layui-card">
+				<?php foreach($data['typeinfo'] as $key => $val){?>
 				<div class="layui-card-header" style="text-align: center;font-size:25px;" id="layui">
-<button type="button" class="layui-btn layui-btn-warm">暖色按钮</button>
+					暖色 暖色 
 				</div>
 				<div class="layui-card-body">
 					<ul class="site-doc-icon">
+					
 						<li>
-							<img height="180px" width="300px" src="<?php echo base_url()?>appoint/vehicle/2020112014404336384.jpg" class="layui-upload-img">
+							<img src="<?php echo base_url()?>appoint/vehicle/2020112014404336384.jpg" class="layui-upload-img">
 							<div class="name">ビーワイディー</div>
 							<div class="layui-right">
-								<input type="checkbox" name="vimg" value="">
+								123
 							</div>
 						</li>
-						<li>
-							<img height="180px" width="300px" src="<?php echo base_url()?>appoint/vehicle/2020112014404336384.jpg" class="layui-upload-img">
-							<div class="name">ビーワイディー</div>
-							<div class="layui-right">
-								<input type="checkbox" name="vimg" value="">
-							</div>
-						</li>
-						<li>
-							<img height="180px" width="300px" src="<?php echo base_url()?>appoint/vehicle/2020112014404336384.jpg" class="layui-upload-img">
-							<div class="name">ビーワイディー</div>
-							<div class="layui-right">
-								<input type="checkbox" name="vimg" value="">
-							</div>
-						</li>
-						<li>
-							<img height="180px" width="300px" src="<?php echo base_url()?>appoint/vehicle/2020112014404336384.jpg" class="layui-upload-img">
-							<div class="name">ビーワイディー</div>
-							<div class="layui-right">
-								<input type="checkbox" name="vimg" value="">
-							</div>
-						</li>
-						<li>
-							<img height="180px" width="300px" src="<?php echo base_url()?>appoint/vehicle/2020112014404336384.jpg" class="layui-upload-img">
-							<div class="name">ビーワイディー</div>
-							<div class="layui-right">
-								<input type="checkbox" name="vimg" value="">
-							</div>
-						</li>
-						<li>
-							<img height="180px" width="300px" src="<?php echo base_url()?>appoint/vehicle/2020112014404336384.jpg" class="layui-upload-img">
-							<div class="name">ビーワイディー</div>
-							<div class="layui-right">
-								<input type="checkbox" name="vimg" value="">
-							</div>
-						</li>
+					
 					</ul>
 				</div>
+				<?php foreach($data['typeinfo'] as $key => $val){?>
 			</div>
 		</div>
 	</div>
