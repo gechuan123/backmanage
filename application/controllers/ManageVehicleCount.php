4e49fae1e4eea06abf79d965e2e26893dae74bb7
DESKTOP-QSKKK56\tengbo <1261492506@QQ.com>
2020-11-27 16:27:56 +0800

    no message

--- application/controllers/ManageVehicleCount.php
+++ application/controllers/ManageVehicleCount.php
@@ -15,12 +15,52 @@ class ManageVehicleCount extends Curd
     protected $returnMsg = ['会員名','密码','メールボックス','電話番号'];
     public function index()
 	{
+
         $this->load->library('session');
 		if(!$this->session->userdata('user_id')){
 			redirect('Pub/login');exit;
 		}
-		echo "111";die;
-		$result = [];
-        return $this->load->view($this->controller . '/list', $result);
+        $params = $_GET;
+        $point = [];
+        for($i=5;$i<=24;$i++){
+            if($i==24){
+                $point[] = '00';
+            }else{
+                if($i<10){
+                     $point[] = '0'.$i;
+                }else{
+                     $point[] = $i;
+                }
+                
+            }
+            
+        }
+        if(!isset($params['d_time'])){
+            $params['d_time'] = date('Y-m-d');
+        }
+
+        $vehicle_type = $this->db->where(['isdelete'=>'0'])->get('ci_manage_vehicle_type')->result_array();
+        $vehicle_arr = [];
+        if(!empty($params['type_id'])){
+             $where['isdelete'] = '0';
+     
+             $where['type_id'] = $params['type_id'];
+             
+             
+             $car_arr = $this->db->where($where)->get('ci_manage_vehicle')->result_array();
+             foreach($car_arr as $k=>$v){
+                 $vehicle_arr[$k]['car_name'] = $v['vehiclename'];
+                 foreach($point as $key=>$value){
+                       $vehicle_user = $this->db->where('vehicle_id',$v['id'])->where('start_time <=',$params['d_time'].' '.$value.':00:00')->where('end_time >=',$params['d_time'].' '.$value.':59:59')->get('ci_manage_vehicle_user')->result_array();
+                       if(empty($vehicle_user)){
+                             $vehicle_arr[$k][$value] = 'x';
+                       }else{
+                             $vehicle_arr[$k][$value] = 'o';
+                       }
+                 }  
+             }
+
+        }
+        return $this->load->view('manage_vehicle_count/list', ['point'=>$point,'vehicle_type'=>$vehicle_type,'vehicle_arr'=>$vehicle_arr,'params'=>$params]);
 	}
 }
--- views/manage_vehicle_count/list.php
+++ views/manage_vehicle_count/list.php
@@ -24,64 +24,66 @@
 <body>
 <div class="x-nav">
 	<span class="layui-breadcrumb"><a href="<?=base_url()?>AppointmentReg">最初のページ</a><a href="javascript:;">リスト</a></span>
-	<a class="layui-btn layui-btn-small" style="line-height:1.6em;margin-top:3px;float:right" href="javascript:location.replace(location.href);" title="更新">
-	<i class="layui-icon" style="line-height:30px">更新</i></a>
+
 </div>
 <div class="x-body">
 
-<div style="line-height:4.2em;text-align:center" >
-	<?php if($prev_moth >= date('Y-m', mktime(null,null,null,date('m',strtotime(date('Y-m',time())))+1,1,date('Y',strtotime(date('Y-m',time())))))){?>
-	<a href="<?=base_url()?>AppointmentSetWeek/index?str=<?=$prev_moth?>" type="button" class="layui-btn"><i class="layui-icon"></i></a>
-	<?php }?>
-    <button type="button" class="layui-btn"><i class="layui-icon"><?=$nowdate?></i></button>
-	<input type="hidden" name="nowdate" value="<?=$nowdate?>" />
-	<a href="<?=base_url()?>AppointmentSetWeek/index?str=<?=$next_moth?>" type="button" class="layui-btn"><i class="layui-icon"></i></a>
-</div>
-	<form class="layui-form" action="javascript:void();">
+	<form class="search-form-inline" action="<?php echo base_url()?>ManageVehicleCount" id="searchform" method="get">
+
+		<div class="layui-input-inline">
+			<select name="type_id">
+			    <?php foreach($vehicle_type as $k=>$v){?>
+				    <option <?php if(isset($params['type_id'])){if($params['type_id']==$v['id']){ echo "selected"; }}?> value="<?php echo $v['id']?>" ><?php echo $v['typename']?></option>
+				<?php }?>
+			</select>
+		</div>
+		<div class="layui-input-inline">
+		    <input type="text" class="layui-input" name="d_time" id="start" placeholder=" - " value="<?php echo $params['d_time']?>" lay-key="11" >
+		</div>
+		<button type="submit" class="layui-btn layui-btn-radius" id="ord_search_bak">検索</button>
+	</form>
+
+
     <table class="layui-table">
         <tbody>
             <tr>
                 <td>
                     <table class="layui-table">
                         <thead>
-							<th style="width:10%"  class="list-center" >日付</th>
-                            <th style="width:10%"  class="list-center" >週間</th>
-                            <th style="width:20%"  class="list-center" >営業日</th>
-							<th style="width:20%"  class="list-center" >営業時間</th>
-                            <th style="width:10%"  class="list-center" >設定</th> 
+							<th style="width:2%"  class="list-center" >車のふた/時間</th>
+                            <?php foreach($point as $k=>$v){?>
+							<th style="width:1%"  class="list-center" ><?php echo $v?>時</th>
+							<?php }?>
                         </thead>
                         <tbody>
-				<?php for($i=1; $i<=count($year_moth_day); $i++){?>
+                            <?php foreach($vehicle_arr as $k=>$v){?>
                             <tr>
-								<td class="list-center"><?=$year_moth_day[$i]['ymd']?></td>
-                                <td class="list-center"><?=$year_moth_day[$i]['weekstr']?></td>
-								<td class="list-center">
-	<?php if(!empty($year_moth_day[$i]['is_work']) && $year_moth_day[$i]['is_work'] == 1){?>
-<input lay-filter="is_work_<?=$i?>" type="radio" name="is_work_<?=$i?>" value="1" title="【営業日】" checked="checked" />
-<input lay-filter="is_work_<?=$i?>" type="radio" name="is_work_<?=$i?>" value="2" title="【休業日】" />
-	<?php }else if(!empty($year_moth_day[$i]['is_work']) && $year_moth_day[$i]['is_work'] == 2){?>
-<input lay-filter="is_work_<?=$i?>" type="radio" name="is_work_<?=$i?>" value="1" title="【営業日】" />
-<input lay-filter="is_work_<?=$i?>" type="radio" name="is_work_<?=$i?>" value="2" title="【休業日】" checked="checked" />
-	<?php }else{?>
-<input lay-filter="is_work_<?=$i?>" type="radio" name="is_work_<?=$i?>" value="1" title="【営業日】" checked="checked" />
-<input lay-filter="is_work_<?=$i?>" type="radio" name="is_work_<?=$i?>" value="2" title="【休業日】" />
-	<?php }?>
-								</td>
-								<td class="list-center">9:00~18:00</td>
-                                <td class="list-center">
+								<td class="list-center"><?php echo $v['car_name']?></td>
+                                <td class="list-center"><?php echo $v['05']?></td>
+								<td class="list-center"><?php echo $v['06']?></td>
+								<td class="list-center"><?php echo $v['07']?></td>
+                                <td class="list-center"><?php echo $v['08']?></td>
+								<td class="list-center"><?php echo $v['09']?></td>
+								<td class="list-center"><?php echo $v['10']?></td>
+								<td class="list-center"><?php echo $v['11']?></td>
+								<td class="list-center"><?php echo $v['12']?></td>
+								<td class="list-center"><?php echo $v['13']?></td>
+								<td class="list-center"><?php echo $v['14']?></td>
+								<td class="list-center"><?php echo $v['15']?></td>
+								<td class="list-center"><?php echo $v['16']?></td>
+								<td class="list-center"><?php echo $v['17']?></td>
+								<td class="list-center"><?php echo $v['18']?></td>
+								<td class="list-center"><?php echo $v['19']?></td>
+								<td class="list-center"><?php echo $v['20']?></td>
+								<td class="list-center"><?php echo $v['21']?></td>
+								<td class="list-center"><?php echo $v['22']?></td>
+								<td class="list-center"><?php echo $v['23']?></td>
+								<td class="list-center"><?php echo $v['00']?></td>
 
-<?php if(!empty($year_moth_day[$i]['is_work']) && $year_moth_day[$i]['is_work'] == 1){?>
-<button class="layui-btn layui-btn-normal layui-btn-radius" title="時間" onclick="x_admin_show('時間','<?php echo base_url()?>AppointmentSetWeek/methodList?pid=<?=$year_moth_day[$i]["ymd"]?>')" ><p style="font-size:14px;color:#FFFFFF">時間</p></button>
-<?php }else if(!empty($year_moth_day[$i]['is_work']) && $year_moth_day[$i]['is_work'] == 2){?>
-【休業日】
-<?php }else{?>
-<button class="layui-btn layui-btn-normal layui-btn-radius" title="時間" onclick="x_admin_show('時間','<?php echo base_url()?>AppointmentSetWeek/methodList?pid=<?=$year_moth_day[$i]["ymd"]?>')" ><p style="font-size:14px;color:#FFFFFF">時間</p></button>
-<?php }?>
-
-                                </td>
                             </tr>
-							<input type="hidden" name="ymd<?=$i?>" value="<?=$year_moth_day[$i]['ymd']?>" />
-				<?php }?>	
+							<?php }?>
+							<input type="hidden"  />
+	
                         </tbody>
                     </table>
                 </td>
@@ -89,12 +91,12 @@
 			
         </tbody>
     </table>
-	</form>
-	<div class="layui-form-item" style="text-align:center">
+
+	<!-- <div class="layui-form-item" style="text-align:center">
 		<div class="layui-input-block">
 			<button class="layui-btn layui-btn-normal layui-btn-radius" id="submitbtn" lay-submit lay-filter="formDemo">設定を執行する</button>
 		</div>
-	</div>
+	</div> -->
 </div>
 <div style="display:none;" id="show">
 	<div class="layui-progress layui-progress-big" lay-filter="demo" lay-showPercent="true">
@@ -103,139 +105,20 @@
 </div>
 <script>
 
-layui.use(['form','element'], function(){
+layui.use(['form','element','laydate'], function(){
 	var form = layui.form;
 	var layer = layui.layer;
 	var element = layui.element;
-	
-	$('#submitbtn').click(function(){
-		layer.confirm('よろしいですか？',{btn:['確定', 'キャンセル'],title:'ノワール大宮予約システム'},function(indexa){
-			parent.layer.close(indexa);
-			var t = 1, fun;
-			var show = layer.open({
-				type: 3,
-				area: ['400px', '100px'],
-				content: $("#show").html(),
-				success: function(){
-					fun = setInterval(function (){
-						element.progress('demo', t + '%');
-						if (t >= 100) { 
-							clearInterval(fun);
-							layer.close(show);
-							location.reload();
-						} else {
-							t++;
-						}
-					}, 30);
-					$.post('<?=base_url()?>Appoint/Record/ajax_create_date ',{nowdate:$("input[name='nowdate']").val()},function(data){});
-				},
-				end: function(){
-					element.progress('demo', '0%');
-					clearInterval(fun);
-				}
-			});
-
-		});
-	});
-
-
-form.on('radio(is_work_1)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd1']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_2)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd2']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_3)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd3']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_4)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd4']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_5)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd5']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_6)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd6']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_7)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd7']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_8)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd8']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_9)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd9']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_10)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd10']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-
-form.on('radio(is_work_11)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd11']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_12)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd12']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_13)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd13']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_14)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd14']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_15)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd15']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_16)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd16']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_17)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd17']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_18)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd18']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_19)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd19']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_20)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd20']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-
-form.on('radio(is_work_21)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd21']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_22)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd22']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_23)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd23']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_24)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd24']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_25)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd25']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_26)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd26']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_27)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd27']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_28)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd28']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_29)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd29']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_30)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd30']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
-form.on('radio(is_work_31)', function(data){$.ajax({
-url:"<?=base_url()?>Appoint/Record/ajax_edit_radio",data:{work:data.value, ymd:$("input[name='ymd31']").val()},type:"post",dataType:"json",
-success:function(data){if(data){location.reload();}}});form.render();});
+	var laydate = layui.laydate;
+	laydate.render({
+        elem: '#start', //指定元素
+        type:'date',
+      
+    });
 
 });
+
+
 
 	
 	
